762b614e1817f3914839824355f71b07c1636f4a
Mateus Neves <mateusneves7777@gmail.com>
2024-07-19 19:41:29 -0400

    commit

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ $(document).ready(() => {
     const enderecoNovaImagem = $("#endereco-nova-imagem").val()
     let novoItem = $("<li style='display:none;'></li>")
 
-    $(`<img src="${enderecoNovaImagem}" alt="">
+    $(`<li style='display: none;'>
+        <img src="${enderecoNovaImagem}" alt="">
         <div class="overlay-imagem-link">
           <a href="${enderecoNovaImagem}" target="_blank">Ver imagem em tamanho real</a>
-        </div>`).appendTo(novoItem)
-    $(novoItem).fadeIn(1000)
-    $(novoItem).appendTo("ul")
-
+        </div>
+      </li>`).appendTo("ul").fadeIn()
+    //$(novoItem).fadeIn(1000)
+    //$(novoItem).appendTo("ul")
     $("#endereco-nova-imagem").val("")
 
   })
